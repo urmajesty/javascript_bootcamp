a9b02747ac4754074d59740abacc50a65129f470
urmajesty <chyrellelewis@gmail.com>
2020-10-12 17:02:21 -0500

    for in object

--- practice_1/practice2.js
+++ practice_1/practice2.js
@@ -6,16 +6,30 @@ const movieReviews = {
 	Madeline: 7
 };
 
-for (let movie of Object.keys(movieReviews)) console.log(movie, movieReviews[movie]);
+// for (let movie of Object.keys(movieReviews)) console.log(movie, movieReviews[movie]);
 
-const ratings = Object.values(movieReviews);
-let total = 0;
-for (let score of ratings) {
-	total += score;
+// const ratings = Object.values(movieReviews);
+// let total = 0;
+// for (let score of ratings) {
+// 	total += score;
+// }
+
+// // total /= ratings.length;
+// // console.log(total);
+
+// let avg = total / ratings.length;
+// console.log(avg);
+
+for (let prop in movieReviews) {
+	console.log(prop);
+	console.log(movieReviews[prop]);
 }
+let total = 0;
+for (let prop in movieReviews) {
+	total += movieReviews[prop];
+}
+console.log(`movie reviews total is: ${total}`);
 
-// total /= ratings.length;
-// console.log(total);
-
-let avg = total / ratings.length;
-console.log(avg);
+for (let k in [ 88, 99, 77, 66 ]) {
+	console.log(k);
+}
