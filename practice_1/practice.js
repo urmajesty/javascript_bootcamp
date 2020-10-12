476277165d33170a596d8d6be7f77652fa18293e
urmajesty <chyrellelewis@gmail.com>
2020-10-12 16:00:29 -0500

    more for of loops

--- practice_1/practice.js
+++ practice_1/practice.js
@@ -1,4 +1,30 @@
-let word = [ 'apple', 'bat', 'car' ];
-for (let char of word) {
-	console.log(char);
+// let word = [ 'apple', 'bat', 'car' ];
+// for (let char of word) {
+// 	console.log(char);
+// }
+
+// for (let char of 'happy') {
+// 	console.log(char.toUpperCase());
+// }
+// const magicSquare = [ [ 3, 5, 2 ], [ 3, 9, 0 ], [ 2, 8, 4 ] ];
+// for (let i = 0; i < magicSquare.length; i++) {
+// 	let row = magicSquare[i];
+// 	let sum = 0;
+// 	for (let j = 0; j < row.length; j++) {
+// 		sum += row[j];
+// 	}
+// 	console.log(`${row} summed to ${sum}`);
+// }
+// for (let row of magicSquare) {
+// 	let sum = 0;
+// 	for (let num of row) {
+// 		sum += num;
+// 	}
+// }
+
+const words1 = [ 'mail', 'milk', 'bath', 'black' ];
+const words2 = [ 'box', 'shake', 'tub', 'power' ];
+
+for (let i = 0; i < words1.length; i++) {
+	console.log(`${words1[i]}${words2[i]}`);
 }
