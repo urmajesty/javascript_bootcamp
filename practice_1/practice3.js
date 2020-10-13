8e33e5536bd40b2ed9dfc567cee0cc5ab47d97a5
urmajesty <chyrellelewis@gmail.com>
2020-10-12 23:51:38 -0500

    conditional functions

--- practice_1/practice3.js
+++ practice_1/practice3.js
@@ -3,15 +3,31 @@
 // 	console.log(`rolled: ${roll}`);
 // }
 
-function throwDice() {
-	rollDie();
-	rollDie();
-	rollDie();
-	rollDie();
-}
+// function throwDice() {
+// 	rollDie();
+// 	rollDie();
+// 	rollDie();
+// 	rollDie();
+// }
 
-function throwDice(num) {
-	for (let i = 0; i < num; i++) {
-		rollDie();
+// function throwDice(num) {
+// 	for (let i = 0; i < num; i++) {
+// 		rollDie();
+// 	}
+// }
+// function sum(x, y) {
+//     console.log(x + y);
+
+// }
+
+// function isGrey(color) {
+// 	return color.toLowerCase() === 'grey';
+// }
+
+function containsGrey(arr) {
+	for (let color of arr) {
+		if (color === 'grey') {
+			return true;
+		}
 	}
 }
